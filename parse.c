77f01ed13e9c8a7953ccaa89112425e93b4a2c43
taiyoslime <t@iyosli.me>
2020-08-06 19:41:58 +0900

    fix parser to handle multiple pipe(|)

--- parse.c
+++ parse.c
@@ -2,46 +2,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "process.h"
 #include "parse.h"
 
 static process *parse(char *buf) {
+	process *prev_proc = NULL;
+	int i = 0;
 
-	process *proc;
+	for(char *buf_l, *_proc_char = strtok_r(buf, "|", &buf_l); _proc_char != NULL; _proc_char = strtok_r(NULL, "|", &buf_l)) {
 
-	if ((proc = (process*) malloc(sizeof(process))) == NULL)
-		return NULL;
+		char *proc_char;
 
-	char **tokens;
-	int tokens_len = 4;
-	if ((tokens = (char**) malloc(sizeof(char*) * tokens_len)) == NULL)
-		return NULL;
-
-	char *token = strtok(buf, " \n");
-	for (int i = 0; token != NULL; i++) {
-		int token_len = strlen(token);
-		if ((tokens[i] = (char*) malloc(sizeof(char) * token_len)) == NULL)
+		if ((proc_char = (char*) malloc(sizeof(char) * strlen(_proc_char))) == NULL)
 			return NULL;
-		memcpy(tokens[i], token, token_len);
 
-		if ( i + 1 == tokens_len ) {
-			tokens_len *= 2;
-			char** tmp = (char**) realloc(tokens, sizeof(char*) * tokens_len);
-			if (tmp == NULL) {
-				free(tokens);
+		memcpy(proc_char, _proc_char, strlen(_proc_char));
+
+		process *proc;
+		if ((proc = (process*) malloc(sizeof(process))) == NULL)
+			return NULL;
+
+		char **tokens;
+		int tokens_len = 4;
+		if ((tokens = (char**) malloc(sizeof(char*) * tokens_len)) == NULL)
+			return NULL;
+
+		char *proc_char_l, *token = strtok_r(proc_char, " \n", &proc_char_l);
+		for (int i = 0; token != NULL; i++) {
+			int token_len = strlen(token);
+			if ((tokens[i] = (char*) malloc(sizeof(char) * token_len)) == NULL)
 				return NULL;
+			memcpy(tokens[i], token, token_len);
+
+			if ( i + 1 == tokens_len ) {
+				tokens_len *= 2;
+				char** tmp = (char**) realloc(tokens, sizeof(char*) * tokens_len);
+				if (tmp == NULL) {
+					free(tokens);
+					return NULL;
+				}
+				tokens = tmp;
+				tmp = NULL;
 			}
-			tokens = tmp;
-			tmp = NULL;
+
+			tokens[i+1] = NULL;
+			token = strtok_r(NULL, " \n", &proc_char_l);
 		}
 
-		tokens[i+1] = NULL;
-		token = strtok(NULL, " \n");
+		proc->arg = tokens;
+		proc->name = proc->arg[0];
+		proc->next = prev_proc;
+
+		prev_proc = proc;
 	}
 
-	proc->arg = tokens;
-	proc->name = proc->arg[0];
-
-	return proc;
+	return prev_proc;
 }
 
 process *read_line() {
